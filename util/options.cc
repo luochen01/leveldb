b5cbb1736b49366e0cf55d61f9cb9095b0fe35fd
luochen <cluo8@uci.edu>
2019-01-24 11:49:26 -0800

    change params

--- util/options.cc
+++ util/options.cc
@@ -16,13 +16,13 @@ Options::Options()
       paranoid_checks(false),
       env(Env::Default()),
       info_log(nullptr),
-      write_buffer_size(4<<20),
+      write_buffer_size(64<<20),
       max_open_files(1000),
       block_cache(nullptr),
       block_size(4096),
       block_restart_interval(16),
-      max_file_size(2<<20),
-      compression(kSnappyCompression),
+      max_file_size(64<<20),
+      compression(kNoCompression),
       reuse_logs(false),
       filter_policy(nullptr) {
 }
